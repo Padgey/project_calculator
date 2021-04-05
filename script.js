dcdba57220280d4cb95b08b3b34d27e4b1538458
Philip <philipadgey@gmail.com>
2021-04-05 09:31:31 +0100

    set up html css and dom skeleton

---
+++ script.js
@@ -0,0 +1,22 @@
+//DOM setup
+const display = document.getElementById("display");
+
+const one = document.getElementById("1");
+const two = document.getElementById("2");
+const three = document.getElementById("3");
+const four = document.getElementById("4");
+const five = document.getElementById("5");
+const six = document.getElementById("6");
+const seven = document.getElementById("7");
+const eight = document.getElementById("8");
+const nine = document.getElementById("9");
+const zero = document.getElementById("0");
+
+const decimal = document.getElementById(".");
+
+const plus = document.getElementById("+");
+const minus = document.getElementById("-");
+const multiply = document.getElementById("x");
+const divide = document.getElementById("/");
+const equals = document.getElementById("=");
+
